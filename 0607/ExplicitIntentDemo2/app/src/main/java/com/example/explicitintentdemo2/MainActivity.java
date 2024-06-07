9494a810887983226d285872a4eec5d6a89e24f3
AlizHsu <A109222043@mail.shu.edu.tw>
2024-06-07 14:36:11 +0800

    0607

--- 0607/ExplicitIntentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
+++ 0607/ExplicitIntentDemo2/app/src/main/java/com/example/explicitintentdemo2/MainActivity.java
@@ -10,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,20 +18,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView output;
-
     ActivityResultLauncher launcher = registerForActivityResult(new ResultContract(),
-        new ActivityResultCallback<Double>() {
+        new ActivityResultCallback<String>() {
             @Override
-            public void onActivityResult(Double o) {
+            public void onActivityResult(String o) {
                 output.setText("計算結果: " + o);
             }
-
-//            @Override
-//            public void onActivityResult(double o) {
-//                output.setText("計算結果: " + o);
-//            }
     });
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,30 +35,27 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //launcher.launch(true);
-                launcher.launch("CHUCK8888");
+                launcher.launch(true);
             }
         });
     }
-    class ResultContract extends ActivityResultContract<String, Double> {
-
+    class ResultContract extends ActivityResultContract<Boolean, String> {
         @NonNull
         @Override
-        public Intent createIntent(@NonNull Context context, String s) {
+        public Intent createIntent(@NonNull Context context, Boolean input) {
             Intent intent = new Intent(MainActivity.this, OpActivity.class);
             Bundle bundle = new Bundle();
             bundle.putString("OPERAND01", ((EditText) findViewById(R.id.txtOpd1)).getText().toString());
             bundle.putString("OPERAND02", ((EditText) findViewById(R.id.txtOpd2)).getText().toString());
             intent.putExtras(bundle);
-            Log.v("CHUCK", s);
             return intent;
         }
 
         @Override
-        public Double parseResult(int i, @Nullable Intent intent) {
+        public String parseResult(int i, @Nullable Intent intent) {
             Bundle bundle = intent.getExtras();
             Double result = bundle.getDouble("RESULT");
-            return result;
+            return result.toString();
         }
     }
 }
